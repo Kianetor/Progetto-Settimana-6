42e323b7b9dafa7ebd5218ddeb718964c38e99ac
Kian Leonardo Jozdani <kianj@libero.it>
2026-06-12 16:42:04 +0200

    Lavoro settimana 6

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,3 +3,17 @@
 // Aggiungi qui il codice JavaScript se serve.
 // Per la Versione Base spesso non serve niente: tutto si fa in HTML+SCSS.
 // Per le versioni Intermedia e Avanzata: filtri lavori, toggle tema, validazione form.
+
+
+const themeToggle = document.getElementById('theme-toggle');
+const themeText = themeToggle.querySelector('.theme-text');
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        themeText.textContent = 'Tema chiaro';
+    } else {
+        themeText.textContent = 'Tema scuro';
+    }
+});
